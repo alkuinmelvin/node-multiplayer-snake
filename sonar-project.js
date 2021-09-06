f039ac1a5bb1f0a972cc1dc2c5b6dfcdf9e45764
Alkuin Melvin <alkuin.melvin@gmail.com>
2021-09-06 20:09:37 +0700

    update

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'http://192.168.56.101:9000',
+       serverUrl: 'http://10.0.142.180:9000',  // your application server ip address (web snake)
        options : {
        'sonar.sources': '.',
        //'sonar.inclusions' : '.' // Entry point of your code
